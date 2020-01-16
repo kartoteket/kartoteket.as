c47d821ada1d7a41afc349196b64d6fd260be6b9
Svale Fossåskaret <svale@3by5.no>
2020-01-16 14:05:25 +0100

    Apply typography and page lyout design

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,23 +1,39 @@
 <template>
-  <main class="pt-16">
-    <h1 class="text-xl uppercase mb-8">
-      About Kartoteket
-    </h1>
-    <div class="rtf">
-      <p>Kartoteket is a studio building websites, visualisations and data driven maps.</p>
-      <p>We write code that creates relations.</p>
-      <p>Relationships enables analysis of patterns, transforming data into knowledge. </p>
-      <p>By connecting the dots, we paint you the picture.</p>
-      <p>
-        That picture might be a map or a data visualization. It might be a website.
-      </p>
-      <p><em>Always, it's storytelling.</em></p>
-
-      <p class="mt-16">
-        Kartoteket where estblished in 2015 with offices currently in Oslo, Norway and  Tel Aviv, Israel
-      </p>
-    </div>
-
-    </div>
-  </main>
+  <article class="pt-16 md:flex">
+    <main class="md:w-2/3 xl:w-1/2 pr-8 mr-8">
+      <h1 class="main-header">
+        About Kartoteket
+      </h1>
+      <div id="lead" class="rtf text-lg leading-relaxed">
+        <p>
+          Kartoteket is a studio building websites, visualisations and data driven maps.
+          We write code that creates relations.
+          Relationships enables analysis of patterns, transforming data into knowledge.
+        </p>
+        <p>
+          By connecting the dots, we paint you the picture.
+          That picture might be a map or a data visualization. It might be a website.
+        </p>
+        <p><em>Always, it's storytelling.</em></p>
+      </div>
+    </main>
+    <aside class="md:w-1/3 xl:w-1/2 self-end mb-16">
+      <h2 class="aside-header">
+        Offices
+      </h2>
+      <div class="rtf">
+        <p>
+          Kartoteket where estblished in 2015 with offices currently in Oslo, Norway and  Tel Aviv, Israel
+        </p>
+      </div>
+      <h2 class="aside-header">
+        Clients
+      </h2>
+      <div class="rtf">
+        <p>
+          Norwegian Ministry of Foreign Affairs, lundhagem architects, IOM Mixed Migration Hub, Rainforest Foundation Norway, The Tax Justice Network Norwar
+        </p>
+      </div>
+    </aside>
+  </article>
 </template>
