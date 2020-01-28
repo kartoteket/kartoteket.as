7e62cf3169017700f302ae25ce6e832b36ebc411
Svale Fossåskaret <svale@3by5.no>
2020-01-28 19:58:33 +0100

    Hook up about page to sanity

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,30 +2,19 @@
   <article class="md:flex">
     <main class="md:w-2/3 xl:w-1/2 md:pr-8 md:mr-8">
       <h1 class="main-header">
-        About Kartoteket
+        {{ entry.title }}
       </h1>
-      <div id="lead" class="rtf md:text-lg leading-relaxed">
-        <p>
-          Kartoteket is a studio building websites, visualisations and data driven maps.
-          We write code that creates relations.
-          Relationships enables analysis of patterns, transforming data into knowledge.
-        </p>
-        <p>
-          By connecting the dots, we paint you the picture.
-          That picture might be a map or a data visualization. It might be a website.
-        </p>
-        <p><em>Always, it's storytelling.</em></p>
-      </div>
+      <block-content v-if="entry.lead" class-name="rtf md:text-lg leading-relaxed" :render-container-on-single-child="true" :blocks="entry.lead" />
+      <block-content v-if="entry.body" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.body" />
     </main>
     <aside class="md:w-1/3 xl:w-1/2 self-end mb-16">
-      <h2 class="aside-header">
-        Offices
-      </h2>
-      <div class="rtf">
-        <p>
-          Kartoteket where estblished in 2015 with offices currently in Oslo, Norway and  Tel Aviv, Israel
-        </p>
-      </div>
+      <article v-for="note in entry.notes" :key="note.id">
+        <h2 class="aside-header">
+          {{ note.title }}
+        </h2>
+        <block-content v-if="note.lead" class-name="rtf" :render-container-on-single-child="true" :blocks="note.lead" />
+        <a v-if="note.body" :href="`notes/${note.slug.current}`">More</a>
+      </article>
       <h2 class="aside-header">
         Clients
       </h2>
@@ -37,3 +26,20 @@
     </aside>
   </article>
 </template>
+<script>
+import BlockContent from 'sanity-blocks-vue-component';
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    BlockContent
+  },
+  async asyncData({ $sanity }) {
+    const query =
+      '{ "entry": *[_type == "page" && slug.current == "about"][0] | {title, slug, lead, body, "notes": note[]->, url}}';
+    const { entry } = await $sanity.fetch(query);
+    console.log(entry);
+    return { entry };
+  }
+};
+</script>
