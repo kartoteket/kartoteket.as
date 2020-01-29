d208e27210e256ab0eaf46b76b09d581e8e2d91a
Svale Fossåskaret <svale@3by5.no>
2020-01-29 15:06:14 +0100

    Cleanup markup semantics

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex flex-col">
-    <header class="mb-8">
+    <header class="main-col mb-8">
       <h1 class="main-header">
         Notes
       </h1>
@@ -8,21 +8,17 @@
         <p>Notes, writings, scribbles. Storytelling</p>
       </div>
     </header>
-    <div class="md:flex flex-1">
-      <main class="md:w-2/3 xl:w-1/2 md:pr-8 md:mr-8">
-        <article v-for="(entry, index) in notes.main" :key="index" class="mb-16 border-t border-white-500">
-          <h1 class="text-2xl mb-2">
-            {{ entry.title }}
-          </h1>
-          <block-content v-if="entry.lead" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.lead" />
-          <a v-if="entry.slug && entry.body" :href="`notes/${entry.slug.current}`">Read more...</a>
-          <a v-if="entry.url" :href="entry.url">Besøk</a>
-        </article>
-      </main>
-      <aside class="md:w-1/3 xl:w-1/2 self-end mb-16">
-        <aside-item v-for="note in notes.side" :key="note.id" :entry="note" />
-      </aside>
-    </div>
+    <article v-for="(entry, index) in notes.main" :key="index" class="main-col mb-16 border-t border-white-500">
+      <h1 class="text-2xl mb-2">
+        {{ entry.title }}
+      </h1>
+      <block-content v-if="entry.lead" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.lead" />
+      <a v-if="entry.slug && entry.body" :href="`notes/${entry.slug.current}`">Read more...</a>
+      <a v-if="entry.url" :href="entry.url">Besøk</a>
+    </article>
+    <aside class="side-col">
+      <aside-item v-for="note in notes.side" :key="note.id" :entry="note" />
+    </aside>
   </section>
 </template>
 <script>
