f87a52f65bac12a9e705bb118f310cbf17bb6f1b
Svale Fossåskaret <svale@3by5.no>
2020-01-29 13:55:44 +0100

    Add template for notes detailed page

---
+++ pages/notes/_id.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="flex flex-col">
+    <header class="main-col mb-8">
+      <h1 class="main-header">
+        {{ entry.title }}
+      </h1>
+      <block-content v-if="entry.lead" class-name="rtf md:text-lg leading-relaxed " :render-container-on-single-child="true" :blocks="entry.lead" />
+    </header>
+    <block-content v-if="entry.body" class-name="main-col rtf mb-16" :render-container-on-single-child="true" :blocks="entry.body" />
+    <aside v-if="entry.notes.length" class="side-col">
+      <aside-item v-for="note in entry.notes" :key="note.id" :entry="note" />
+    </aside>
+  </article>
+</template>
+<script>
+import asideItem from '@/components/asideItem';
+export default {
+  components: {
+    asideItem
+  },
+  async asyncData({ $sanity, params }) {
+    const filters = [
+      '[_type == "note"]',
+      `[slug.current == "${params.id}"]`,
+      '[0]'
+    ];
+    const projection = ['{title, slug, lead, body, url,"notes": note[]->}'];
+    const query = `{
+      "entry": ${['*']
+        .concat(filters)
+        .concat([projection])
+        .join('|')}
+      }`;
+    const result = await $sanity.fetch(query);
+    return result;
+  }
+};
+</script>
