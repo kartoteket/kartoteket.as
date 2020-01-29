13e786a0e31951b5288eb3301f5b179034dc4f03
Svale Fossåskaret <svale@3by5.no>
2020-01-29 13:56:24 +0100

    Clean up markup semantics

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <article class="md:flex">
-    <main class="md:w-2/3 xl:w-1/2 md:pr-8 md:mr-8 mb-16">
+  <article class="flex flex-col">
+    <header class="main-col mb-8">
       <h1 class="main-header">
         {{ entry.title }}
       </h1>
-      <block-content v-if="entry.lead" class-name="rtf md:text-lg leading-relaxed" :render-container-on-single-child="true" :blocks="entry.lead" />
-      <block-content v-if="entry.body" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.body" />
-    </main>
-    <aside class="md:w-1/3 xl:w-1/2 self-end">
+      <block-content v-if="entry.lead" class-name="rtf md:text-lg leading-relaxed " :render-container-on-single-child="true" :blocks="entry.lead" />
+    </header>
+    <block-content v-if="entry.body" class-name="main-col rtf mb-16" :render-container-on-single-child="true" :blocks="entry.body" />
+    <aside class="side-col">
       <aside-item v-for="note in entry.notes" :key="note.id" :entry="note" />
       <aside-item :entry="clients" />
     </aside>
