cd924eac7d80ae54050363500b58a0bfe6c442fb
Svale Fossåskaret <svale@3by5.no>
2020-02-04 12:21:04 +0100

    Better handling of internal/extranal links

--- components/asideItem.vue
+++ components/asideItem.vue
@@ -1,7 +1,9 @@
 <template>
   <article class="mb-6">
     <h2 class="aside-header">
-      {{ entry.title }}
+      <link-wrapper :to="link(entry)">
+        {{ entry.title }}
+      </link-wrapper>
     </h2>
     <block-content v-if="entry.lead" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.lead" />
     <ul v-if="entry.list" class="rtf">
@@ -10,16 +12,29 @@
         <a :href="item.url">{{ item.name }}</a>
       </li>
     </ul>
-    <a v-if="entry.slug && entry.body" class="link" :href="`notes/${entry.slug.current}`">Read more</a>
-    <a v-if="entry.url" class="link" :href="entry.url">Read more</a>
+    <link-wrapper v-if="link(entry)" class="link" :to="link(entry)">
+      Read more...
+    </link-wrapper>
   </article>
 </template>
 <script>
+import linkWrapper from '@/components/linkWrapper';
+
 export default {
+  components: {
+    linkWrapper
+  },
   props: {
     entry: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    link(e) {
+      if (e.slug && e.body) return `notes/${e.slug.current}`;
+      if (e.url) return e.url;
+      return null;
     }
   }
 };
--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -10,11 +10,14 @@
     </header>
     <article v-for="(entry, index) in notes.main" :key="index" class="main-col mb-16 border-t border-white-500">
       <h1 class="text-2xl mb-2">
-        {{ entry.title }}
+        <link-wrapper :to="link(entry)">
+          {{ entry.title }}
+        </link-wrapper>
       </h1>
-      <block-content v-if="entry.lead" class-name="rtf" :render-container-on-single-child="true" :blocks="entry.lead" />
-      <a v-if="entry.slug && entry.body" :href="`notes/${entry.slug.current}`">Read more...</a>
-      <a v-if="entry.url" :href="entry.url">Besøk</a>
+      <block-content v-if="entry.lead" class-name="rtf mb-2" :render-container-on-single-child="true" :blocks="entry.lead" />
+      <link-wrapper v-if="link(entry)" class="link" :to="link(entry)">
+        Read more...
+      </link-wrapper>
     </article>
     <aside class="side-col">
       <aside-item v-for="note in notes.side" :key="note.id" :entry="note" />
@@ -24,15 +27,18 @@
 <script>
 import head from '~/mixins/head.js';
 import asideItem from '@/components/asideItem';
+import linkWrapper from '@/components/linkWrapper';
 import {
   webSite,
   organisation,
   webPage,
   breadCrumbs
 } from '@/utils/structureddata.js';
+
 export default {
   components: {
-    asideItem
+    asideItem,
+    linkWrapper
   },
   mixins: [head],
   data() {
@@ -90,6 +96,13 @@ export default {
     }`;
     const result = await $sanity.fetch(query);
     return { notes: result };
+  },
+  methods: {
+    link(e) {
+      if (e.slug && e.body) return `notes/${e.slug.current}`;
+      if (e.url) return e.url;
+      return null;
+    }
   }
 };
 </script>
