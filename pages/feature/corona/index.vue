2d4f428a2f5f4600f70d072c9a2901b6a71476c9
Svale Fossåskaret <svale@3by5.no>
2020-03-02 10:54:09 +0100

    Cleaning up the page componenet Moved a lot of stuff to chart components Prepped for pre-publish

--- pages/feature/corona/index.vue
+++ pages/feature/corona/index.vue
@@ -2,12 +2,19 @@
   <article class="flex flex-col">
     <header class="main-col mb-8">
       <h1 class="main-header">
-        Corona virus COVID-19
+        {{ page.title }}
       </h1>
       <div class="rtf md:text-lg leading-relaxed">
-        DataSource: <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">Johns Hopkins CSSE</a>
+        <p>{{ page.description }}</p>
+      </div>
+      <div class="rtf">
+        <p>You can see the details of a specific country by selecting it in the first chart country selector.</p>
+        <p>Data Source: <a href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">Johns Hopkins CSSE</a> (<a href="https://github.com/CSSEGISandData/COVID-19">gitHub files</a>)</p>
       </div>
     </header>
+    <article>
+      <plot-map-chart v-if="false" :chart-data="chartData" />
+    </article>
     <div class="flex flex-wrap">
       <article class="lg:w-1/2 mb-12">
         <v-select v-model="selection" class="dropdown mx-8" :options="countriesList" value="{default:'Norway'}" />
@@ -18,7 +25,6 @@
         <multi-line-chart :id="i+1" :series="set.data" />
       </article>
     </div>
-    </div>
   </article>
 </template>
 <script>
@@ -27,15 +33,13 @@
 import * as d3Lib from 'd3'; // @todo cherrypick like this: var d3 = Object.assign({}, require("d3-format"), require("d3-geo"), require("d3-geo-projection"));
 import * as d3Array from 'd3-array';
 import * as moment from 'moment';
-import * as topojson from 'topojson-client';
 
 import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
 
-import topo from './world2.topojson.json';
-
 import head from '~/mixins/head.js';
 import MultiLineChart from '@/components/charts/MultiLineChart';
+import PlotMapChart from '@/components/charts/PlotMapChart';
 
 import {
   webSite,
@@ -49,6 +53,7 @@ const d3 = Object.assign({}, d3Lib, d3Array);
 export default {
   components: {
     MultiLineChart,
+    PlotMapChart,
     vSelect
   },
   mixins: [head],
@@ -66,18 +71,18 @@ export default {
         bottom: 10
       },
       page: {
-        title: 'About Kartoteket',
-        slug: 'feature/corona',
+        title: 'Corona - Daily COVID-19 registrations',
+        slug: 'features/corona/daily',
         description:
-          'Kartoteket is a studio that creates websites, data visualisations and data driven maps. We specialize in performance, accessibility and SEO',
-        url: `https://kartoteket.as/feature/corona`
+          'Charts showing the timeline trend of new daily registered confirmed cases of COVID-19 in different countries.',
+        url: `https://kartoteket.as/features/corona/daily`
       }
     };
   },
   computed: {
     chartSeries() {
       const scandinavia = {
-        title: 'Scandinavia',
+        title: 'Nordic Countries',
         data: this.getSeries(
           this.getCountries(['Norway', 'Sweden', 'Denmark', 'Finland'])
         )
@@ -94,54 +99,18 @@ export default {
           ])
         )
       };
-      const World = {
+      const US = {
         title: 'US',
         data: this.getSeries(this.getCountries('US'))
       };
 
-      return [scandinavia, keyCountries, World];
+      return [scandinavia, keyCountries, US];
     },
     width() {
       return d3.min([300, 1600]); // @todo: get clientWidth
     },
     height() {
       return d3.min([this.width * 0.5, 800]);
-    },
-    x() {
-      return d3
-        .scaleTime()
-        .domain(d3.extent(this.dataset, d => moment(d.date))) // d3.extent(data, d => parseDate(d.LastUpdate))
-        .range([this.margin.left, this.width - this.margin.right]);
-    },
-    y() {
-      // sort by length - inside out
-      const arr = [];
-      const copy = this.daysCount.slice();
-      copy.sort((a, b) => a.length - b.length);
-      copy.forEach((e, i) => {
-        if (i % 2) arr.unshift(e);
-        else arr.push(e);
-      });
-      return d3
-        .scaleBand()
-        .domain(arr.map(d => d.name) /* .sort(d3.descending) */)
-        .range([this.height - this.margin.bottom, this.margin.top])
-        .paddingInner(1)
-        .paddingOuter(1)
-        .align(0.5)
-        .round(true);
-    },
-    yCountry() {
-      return d3
-        .scaleLinear()
-        .domain([0, d3.max(this.dataset, d => d.confirmed) + 30])
-        .range([this.height - this.margin.bottom, this.margin.top]);
-    },
-    color() {
-      return d3
-        .scaleOrdinal()
-        .domain(['confirmed', 'recovered', 'deaths'])
-        .range(['#2B3F4E', '#AEC84B', '#D14A51']);
     },
     parsedData() {
       // create copy (ref: https://observablehq.com/@tmcw/observable-anti-patterns-and-code-smells#mutation )
@@ -164,17 +133,14 @@ export default {
       return this.addDailyValues(this.parsedData.data);
     },
     chartData() {
-      return this.view === 'map' ? this.latest : this.dataset;
-    },
-    latest() {
-      // @todo: should also be a method
-      return this.dataset
-        .filter(d => moment(d.date).isSame(this.dates[this.dates.length - 1]))
-        .sort((a, b) => d3.ascending(a.length, b.length));
+      return [];
+      // return this.view === 'map'
+      //   ? this.getLatest(this.getCountries())
+      //   : this.getCountries();
     },
     dates() {
-      return Array.from(new Set(this.dataset.map(d => d.date)))
-        .map(d => moment(d))
+      return Array.from(new Set(this.getCountries().map(d => d.date)))
+        .map(d => moment(d, 'M/D/YY'))
         .sort(d3.ascending);
     },
     series() {
@@ -187,24 +153,6 @@ export default {
       return Array.from(new Set(this.input.map(d => d.country))).sort(
         d3.ascending
       );
-    },
-    countries() {
-      // @todo: Noe need to double this. Move filter to where it is used
-      return this.land.features.filter(
-        d => !d.properties.NAME.includes('tarctica')
-      );
-    },
-    land() {
-      return topojson.feature(topo, topo.objects.countries);
-    },
-    projection() {
-      return d3
-        .geoNaturalEarth1()
-        .fitExtent([[0, 0], [this.width, this.height]], this.land)
-        .translate([this.Width / 2, this.height]);
-    },
-    path() {
-      return d3.geoPath(this.projection);
     },
     structuredData() {
       return {
@@ -295,6 +243,11 @@ export default {
         };
       });
     },
+    getLatest(input) {
+      return input
+        .filter(d => moment(d.date).isSame(this.dates[this.dates.length - 1]))
+        .sort((a, b) => d3.ascending(a.length, b.length));
+    },
     getCountries(input) {
       let selection = [];
       if (input) {
@@ -378,232 +331,6 @@ export default {
         });
       }
       return Array.from(grouped, ([key, value]) => value).flat();
-    },
-    // SCALES
-    size(selection) {
-      const domain = this.input.filter(d => {
-        if (selection === 'World' || selection === 'Mainland China')
-          return true;
-        return d.country !== 'Mainland China';
-      });
-
-      const range = this.view === 'chart' ? [2, 25] : [2, 65];
-
-      return d3
-        .scaleSqrt()
-        .domain(d3.extent(domain, d => d.confirmed))
-        .range(range);
-    },
-    // AXIS
-    xAxis(svg) {
-      const interval = this.width > 600 ? 10 : 3;
-      return svg.attr('transform', `translate(0,${this.margin.top})`).call(
-        d3
-          .axisTop(this.x)
-          .ticks(interval)
-          .tickFormat(d3.timeFormat('%d.%m'))
-          .tickSizeOuter(0)
-          .tickSizeInner(
-            (this.height - this.margin.top - this.margin.bottom) * -1
-          )
-      );
-    },
-    yAxis(svg, y) {
-      return svg
-        .attr('transform', `translate(${this.margin.left - 20},0)`)
-        .call(
-          d3
-            .axisLeft(y)
-            .tickSizeOuter(0)
-            .tickSizeInner(
-              (this.width - this.margin.right - this.margin.left) * -1
-            )
-        );
-    },
-    // HELPERS
-    yScaleKey(dimension) {
-      if (this.countriesList.includes(this.selection)) {
-        return dimension;
-      }
-      return 'name';
-    },
-    showTooltip(d, g) {
-      g.style('display', null)
-        .style('pointer-events', 'none')
-        .style('font', '12px Helvetica, arial, sans-serif');
-
-      g.selectAll('text')
-        .data([null])
-        .join('text')
-        .attr(
-          'transform',
-          `translate(${d3.event.pageY - 10}, ${d3.event.pageX})`
-        )
-        .call(text =>
-          text
-            .selectAll('tspan')
-            .attr('fill', '#777')
-            .datum(d)
-            .join('tspan')
-            .attr('y', (d, i) => `${i * 1.2}em`)
-            .style('font-weight', (_, i) => (i ? null : 'bold'))
-            .text(d => `${d.name} ${d.value}`)
-        );
-    },
-    removeTooltip() {
-      const tooltip = d3.select('.svg-tooltip');
-      return tooltip.style('visibility', 'hidden');
-    },
-    addVoronoi(g, generator, toolTip) {
-      g.selectAll('path')
-        .data(generator(this.chartData).polygons())
-        .enter()
-        .append('path')
-        .attr('d', (d, i) => 'M' + d.join('L') + 'Z')
-        .datum((d, i) => d.data)
-        // Give each cell a unique class where the unique part corresponds to the circle classes
-        .attr('class', (d, i) => `cid-${i}`)
-        // .style("stroke", "#2074A0") //If you want to look at the cells
-        .style('fill', 'none')
-        .style('pointer-events', 'all')
-        .on('mouseover', d => this.showTooltip(d, toolTip))
-        .on('mouseout', d => this.removeTooltip(d, toolTip));
-    },
-    chartVoronoi() {
-      d3.voronoi()
-        .x(function(d) {
-          return this.x(moment(d.date));
-        })
-        .y(function(d) {
-          return this.y(d.name);
-        })
-        .extent([[0, 0], [this.width, this.height]]);
-    },
-    mapVoronoi() {
-      d3.voronoi()
-        .x(d => this.projection(d.pos)[0])
-        .y(d => this.projection(d.pos)[1])
-        .extent([[0, 0], [this.width, this.height]]);
-    },
-    dots(selection, t, dimension) {
-      selection.join(
-        enter =>
-          enter
-            .append('circle')
-            .attr('class', d => d.name)
-            .style('opacity', 0)
-            .attr('r', 0)
-            //        .attr("transform", `translate(${width / 2},${height})`)
-            .attr(
-              'transform',
-              d => `translate(${this.x(moment(d.date))},${this.y(d.name)})`
-            )
-            .style('fill', d => this.color(dimension))
-            .style('stroke', d => this.color(dimension))
-            .call(enter =>
-              enter
-                .transition(t)
-                .style('opacity', 0.75)
-                .style('fill-opacity', 0.25)
-            ),
-        update => update,
-        exit =>
-          exit.call(exit =>
-            exit
-              .transition(t)
-              .attr('r', 0)
-              .style('opacity', 0)
-              .remove()
-          )
-      );
-      //    .on("mouseover", function(d) {
-      //     console.log('m over');
-      //  });
-    },
-    legend() {
-      const values = [
-        this.size.domain()[0],
-        this.size.domain()[1] * 0.25,
-        this.size.domain()[1] * 0.5,
-        this.size.domain()[1]
-      ];
-      return this.legendCircle()
-        .tickValues(values)
-        .tickFormat(
-          (d, i, e) => (i === e.length - 1 ? d + ' confirmed cases' : d)
-        )
-        .scale(this.size)
-        .tickSize(5);
-    },
-    // By Harry Stevens - Source: https://observablehq.com/@harrystevens/circle-legend#legendCircle
-    legendCircle(context) {
-      let scale;
-      let tickValues;
-      let tickFormat = d => d;
-      let tickSize = 5;
-
-      function legend(context) {
-        let g = context.select('g');
-        if (!g._groups[0][0]) {
-          g = context.append('g');
-        }
-        g.attr('transform', `translate(${[1, 1]})`);
-
-        const ticks = tickValues || scale.ticks();
-
-        const max = ticks[ticks.length - 1];
-
-        g.selectAll('circle')
-          .data(ticks.slice().reverse())
-          .enter()
-          .append('circle')
-          .attr('fill', 'none')
-          .attr('stroke', 'currentColor')
-          .attr('cx', scale(max))
-          .attr('cy', scale)
-          .attr('r', scale);
-
-        g.selectAll('line')
-          .data(ticks)
-          .enter()
-          .append('line')
-          .attr('stroke', 'currentColor')
-          .attr('stroke-dasharray', '4, 2')
-          .attr('x1', scale(max))
-          .attr('x2', tickSize + scale(max) * 2)
-          .attr('y1', d => scale(d) * 2)
-          .attr('y2', d => scale(d) * 2);
-
-        g.selectAll('text')
-          .data(ticks)
-          .enter()
-          .append('text')
-          .attr('font-family', "'Helvetica Neue', sans-serif")
-          .attr('font-size', 11)
-          .attr('dx', 3)
-          .attr('dy', 4)
-          .attr('x', tickSize + scale(max) * 2)
-          .attr('y', d => scale(d) * 2)
-          .text(tickFormat);
-      }
-
-      legend.tickSize = function(_) {
-        return arguments.length ? ((tickSize = +_), legend) : tickSize;
-      };
-
-      legend.scale = function(_) {
-        return arguments.length ? ((scale = _), legend) : scale;
-      };
-
-      legend.tickFormat = function(_) {
-        return arguments.length ? ((tickFormat = _), legend) : tickFormat;
-      };
-
-      legend.tickValues = function(_) {
-        return arguments.length ? ((tickValues = _), legend) : tickValues;
-      };
-
-      return legend;
     }
   }
 };
