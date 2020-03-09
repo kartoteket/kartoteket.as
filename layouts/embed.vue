8ee27c3bb074bb6a7e4a48e2817fce0241aa4835
Svale Fossåskaret <svale@3by5.no>
2020-03-09 22:41:56 +0100

    WIP: Setting up a simple embedable trendline chart

---
+++ layouts/embed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="site-container font-light">
+    <nuxt class="p-page flex-1" />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'theme-light'
+    }
+  }
+};
+</script>
